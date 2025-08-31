f6a6ed1cef4cd10e980e535d01c9a43bf8030bf9
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-08-31 19:21:44 -0400

    tut

--- test.cpp
+++ test.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+
+/* PRINTING to the TERMINAL
+
 int main() {
     std::cout << "Hello World";
     return 0;
 }
+*/
+
+/* variables + outputing values
+int main() {
+    int file_size = 100;
+    double sales = 9.99;
+    std::cout << file_size;
+    return 0;
+}
+*/
+
